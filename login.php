9749916f519f792d1eb5a8a2d442331140288143
edinabwari <bwarikwambokaedina@gmail.com>
2025-07-12 23:15:03 +0300

    Updated the log in form

--- login.php
+++ login.php
@@ -7,11 +7,10 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body class="login-body">
-      
+<body class="login-body">      
       <form method="POST" action="app/login.php" class="shadow p-4">
 
-      	  <h3 class="display-4">Login Formls</h3>
+      	  <h3 class="display-4">Login Form</h3>
       	  <?php if (isset($_GET['error'])) {?>
       	  	<div class="alert alert-danger" role="alert">
 			  <?php echo htmlspecialchars($_GET['error']); ?>
