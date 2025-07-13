018d5e699c7172af7e35c38a543dd7ad2d2fadd9
edinabwari <bwarikwambokaedina@gmail.com>
2025-07-13 19:53:47 +0300

    Updated the log in form and handled errors

--- login.php
+++ login.php
@@ -5,12 +5,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login Page</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body class="login-body">      
       <form method="POST" action="app/login.php" class="shadow p-4">
+		<div class="login-icon text-center mb-3">
+           <span class="fas fa-hands-helping fa-4x text-secondary"></span>
+                </div>
 
       	  <h3 class="display-4">Login Form</h3>
+		  
       	  <?php if (isset($_GET['error'])) {?>
       	  	<div class="alert alert-danger" role="alert">
 			  <?php echo htmlspecialchars($_GET['error']); ?>
@@ -25,7 +30,7 @@
      
       	  ?>			
 		  <div class="mb-3">
-		    <label for="exampleInputEmail1" class="form-label">User name</label>
+		    <label for="exampleInputEmail1" class="form-label">User Name</label>
 		    <input type="text" class="form-control" name="user_name">
 		  </div>
 		  <div class="mb-3">
@@ -36,7 +41,7 @@
 		    <label for="exampleInputPassword1" class="form-label">Password</label>
 		    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
 		  </div>
-		  <button type="submit" class="btn btn-primary">Login</button>
+		  <button type="submit" class="btn btn-info">Login</button>
 		</form>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
