618ce4a6548c8acffc6450b4f12444348e9ce168
edinabwari <bwarikwambokaedina@gmail.com>
2025-07-14 07:02:33 +0300

    Updated db

--- DB_connection.php
+++ DB_connection.php
@@ -1,9 +1,13 @@
 <?php  
-
-$sName = "sql102.infinityfree.com"; 
-$uName = "if0_39457663";            
-$pass  = "KehkYWVVL5ou"; 
-$db_name = "if0_39457663_task_management_db"; 
+$sName = "localhost";
+$uName = "root";
+$pass  = "";
+$db_name = "task_management_db";
+//For the purpose of hosting 
+// $sName = "sql102.infinityfree.com"; 
+// $uName = "if0_39457663";            
+// $pass  = "KehkYWVVL5ou"; 
+// $db_name = "if0_39457663_task_management_db"; 
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
